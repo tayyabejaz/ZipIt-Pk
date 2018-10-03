5e2ede6e3de16f0ee9f9952c11038c74add025cf
Tayyab Ejaz <tayyabejaz@macbooks-macbook-pro.local>
2018-10-03 15:01:22 +0500

    Initial Commit

---
+++ ZipIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZipIt
+//
+//  Created by Tayyab Ejaz on 03/10/2018.
+//  Copyright © 2018 Tayyab Ejaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
